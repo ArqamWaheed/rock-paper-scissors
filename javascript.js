6ca08349b659d7fb1de685846efde20f14e8095f
ArqamWaheed <86839084+ArqamWaheed@users.noreply.github.com>
2024-06-01 12:20:28 +0500

    Finished the project, yet to add design.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,32 @@
+const btns = document.querySelectorAll(".buttons button");
+const score = document.querySelectorAll(".result h1");
+let humanChoice = "";
 let humanScore = 0;
 let computerScore = 0;
+
+const output = document.createElement("p")
+output.style.cssText = "font-size: 80px; text-align: center; margin-top: 10vh;"
+document.querySelector("#container").appendChild(output)
+
+
+btns.forEach(function(e) {
+    e.addEventListener('click', () => {
+        switch(e.getAttribute("id")) {
+            case "rock":
+                humanChoice = "Rock";
+                playRound(getComputerChoice());
+                break;
+            case "paper":
+                humanChoice = "Paper";
+                playRound(getComputerChoice());
+                break;
+            case "scissors":
+                humanChoice = "Scissors";
+                playRound(getComputerChoice());
+                break;
+        }
+    })
+});
 
 function getComputerChoice() {
     let randomizer = () => Math.floor(Math.random() * 3);
@@ -13,48 +40,50 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let selector = () => prompt("Choose R for Rock, P for Paper, or S for Scissors").toUpperCase();
-    let choice = selector();
-    if (choice === "R") {
-        return "Rock";
-    } else if (choice === "P") {
-        return  "Paper";
-    } else if (choice === "S") {
-        return "Scissors"; 
-    } else {
-        return alert("Invalid option");
+
+function playRound(computerChoice) {
+    if (computerChoice === humanChoice) {
+        output.textContent = `It's a TIE! ${humanChoice} can't beat ${computerChoice}`;
+    }  else if (humanChoice === "Rock" && computerChoice === "Scissors") {
+        output.textContent = `You WIN! ${humanChoice} beats ${computerChoice}`;
+        humanScore += 1;
+    }  else if (humanChoice === "Scissors" && computerChoice === "Paper") {
+        output.textContent = `You WIN! ${humanChoice} beats ${computerChoice}`;
+        humanScore += 1;
+    }   else if (humanChoice === "Paper" && computerChoice === "Rock") {
+        output.textContent = `You WIN! ${humanChoice} beats ${computerChoice}`;
+        humanScore += 1;
+    }   else {
+        output.textContent = `You LOSE! ${humanChoice} loses to ${computerChoice}`;
+        computerScore += 1;
     }
+    updateScore();
+    checkScore();
 }
 
-
-function playGame() {
-    function playRound(humanChoice, computerChoice) {
-        if (computerChoice === humanChoice) {
-            console.log(`It's a TIE! ${humanChoice} can't beat ${computerChoice}`);
-        }  else if (humanChoice === "Rock" && computerChoice === "Scissors") {
-            console.log(`You WIN! ${humanChoice} beats ${computerChoice}`);
-            humanScore += 1;
-        }  else if (humanChoice === "Scissors" && computerChoice === "Paper") {
-            console.log(`You WIN! ${humanChoice} beats ${computerChoice}`);
-            humanScore += 1;
-        }   else if (humanChoice === "Paper" && computerChoice === "Rock") {
-            console.log(`You WIN! ${humanChoice} beats ${computerChoice}`);
-            humanScore += 1;
-        }   else {
-            console.log(`You LOSE! ${humanChoice} loses to ${computerChoice}`);
-            computerScore += 1;
+function updateScore() {
+    score.forEach((e) => {
+        if (e.textContent[0] == "Y") {
+            e.textContent = e.textContent.slice(0,-1) + humanScore;
+            
+        } else {
+            e.textContent = e.textContent.slice(0, -1) + computerScore;
         }
-    }
-    while (computerScore < 5 && humanScore < 5) {
-        let computerSelection = getComputerChoice();
-        let humanSelection = getHumanChoice();  
-        playRound(humanSelection, computerSelection);
+    })
+}
+
+function checkScore() {
+    if (computerScore >= 5) {
+        output.textContent = `Game has ended, you have LOST! Choose an option again to restart!`;
+        computerScore = 0;
+        humanScore = 0;
+    } else if (humanScore >= 5) {
+        output.textContent = `Game has ended, you have WON! Choose an option again to restart`;
+        computerScore = 0;
+        humanScore = 0;
     }
 }
 
-
-playGame();
 
 
 
